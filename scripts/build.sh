ce15c2328f88e86fe0c888b2ab717b5351fe44de
wltan <53135010+wltan@users.noreply.github.com>
2020-06-17 16:10:49 +0800

    Extract I/O pipes to base

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ../dockerfiles
-sudo docker build -t base -f base-dockerfile .
+sudo docker build -t base -f base-dockerfile ./context
 for IMAGE_NAME in $(cat ../image-names.txt)
 do
     sudo docker build -t $IMAGE_NAME -f $IMAGE_NAME ./context
