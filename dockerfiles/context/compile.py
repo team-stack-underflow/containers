02da401000430cc19389ad130332269c696f58ab
wltan <53135010+wltan@users.noreply.github.com>
2020-06-24 16:41:27 +0800

    Add compile mode images

---
+++ dockerfiles/context/compile.py
@@ -0,0 +1,23 @@
+from os import environ
+import boto3
+import subprocess
+
+sqs = boto3.client('sqs')
+
+queue_url = f"https://sqs.us-east-1.amazonaws.com/%s/%s-output" % (environ["USER_ID"], environ["RUN_ID"])
+
+if __name__ == "__main__":
+    compile = subprocess.run(environ["COMPILE_CMD"].split(), capture_output=True, text=True)
+    if compile.returncode != 0:
+        sqs.send_message(
+            QueueUrl=queue_url,
+            MessageBody=compile.stderr,
+            MessageAttributes={
+                "client": {
+                    "StringValue": environ["RUN_ID"],
+                    "DataType": "String"
+                }
+            }
+        )
+    exit(compile.returncode)
+    
---
+++ dockerfiles/context/download.py
@@ -0,0 +1,20 @@
+from os import environ
+import boto3
+
+sqs = boto3.client('sqs')
+
+queue_url = f"https://sqs.us-east-1.amazonaws.com/%s/%s-input" % (environ["USER_ID"], environ["RUN_ID"])
+
+if __name__ == "__main__":
+    response = sqs.receive_message(
+        QueueUrl=queue_url,
+        MaxNumberOfMessages=1,
+        WaitTimeSeconds=20
+    )
+    msg = response["Messages"][0]
+    with open(environ["SRC_FILE"], "w") as src:
+        src.write(msg["Body"])
+    sqs.delete_message(
+        QueueUrl=queue_url,
+        ReceiptHandle=msg["ReceiptHandle"]
+    )
